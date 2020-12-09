9949a182a46efa84d11c3f9ea9a7c875afe406c6
takumi9332 <takumi9332@gmail.com>
2020-12-09 16:04:17 +0900

    ActiveStorageとActiveHash導入

---
+++ app/models/genre.rb
@@ -0,0 +1,17 @@
+class Genre < ActiveHash::Base
+  self.data = [
+    { id: 1, name: '--' },
+    { id: 2, name: '' },
+    { id: 3, name: '' },
+    { id: 4, name: '' },
+    { id: 5, name: '' },
+    { id: 6, name: '' },
+    { id: 7, name: '' },
+    { id: 8, name: '' },
+    { id: 9, name: '' },
+    { id: 10, name: '' }
+  ]
+
+  include ActiveHash::Associations
+  has_many :products
+  end
