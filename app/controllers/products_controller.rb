3cbd228e6db70499466021718b7a91bb3f282822
takumi9332 <takumi9332@gmail.com>
2020-12-11 20:24:47 +0900

    商品削除機能の修正

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,5 @@
 class ProductsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :edit]
+  before_action :authenticate_user!, only: [:new, :edit, :destroy]
   before_action :set_product, only: [:show, :edit, :update, :destroy]
 
   def index
