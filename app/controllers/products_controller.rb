c37b0493e8f0ce5e577b5c2d9758cae64099ca60
takumi9332 <takumi9332@gmail.com>
2020-12-11 12:10:41 +0900

    商品一覧表示の修正

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,8 +1,7 @@
 class ProductsController < ApplicationController
   before_action :authenticate_user!, only: :new
   def index
-    @products = Product.all
-    @products = Product.order("created_at DESC")
+    @products = Product.includes(:user).order("created_at DESC")
   end
 
   def new
