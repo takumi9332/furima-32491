a2c7676fbf18446203cc987907c71e7ecb1f0814
takumi9332 <takumi9332@gmail.com>
2020-12-12 19:35:26 +0900

    フォームの情報を保存&エラーメッセージ表示

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,19 +1,22 @@
 class PurchasesController < ApplicationController
   def index
-    @purchaser = Purchaser.new
+    @purchaser = PurchaserForm.new
+    @product = Product.find(params[:product_id])
   end
+
   def create
-    @purchaser = Purchaser.new(purchaser_params)
+    @purchaser = PurchaserForm.new(purchaser_params)
+    @product = Product.find(params[:product_id])
     if @purchaser.valid?
       @purchaser.save
-      redirect_to action: :index
+      redirect_to root_path
     else
-      render action: :new
+      render :index
     end
   end
 
   private
   def purchaser_params
-    params.require(:purchaser).permit(:postal_code, :prefecture_id, :city, :address, :building, :purchase_id).merge(user_id: current_user.id, product_id: params[:product_id])
+    params.require(:purchaser_form).permit(:postal_code, :prefecture_id, :city, :address, :building, :phone_number).merge(user_id: current_user.id, product_id: params[:product_id], purchase_id: params[:purchase_id])
   end
 end
--- app/models/purchaser_form.rb
+++ app/models/purchaser_form.rb
@@ -1,20 +1,20 @@
-class Purchaser
+class PurchaserForm
   include ActiveModel::Model
-  after_accessor :user, :product, :postal_code, :prefecture_id, :city, :address, :building, :phone_number, :purchase
+  attr_accessor :user_id, :product_id, :postal_code, :prefecture_id, :city, :address, :building, :phone_number, :purchase_id
   
   with_options presence: true do
-    validates :user
-    validates :product
+    validates :user_id
+    validates :product_id
     validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "ハイフン(-)を使用してください"}
     validates :city
     validates :address
     validates :phone_number, format: {with: /\A[0-9]{11}\z/}
-    validates :purchase
+    validates :purchase_id
   end
   validates :prefecture_id, numericality: { other_than: 1 }
 
   def save
-    Purchases.create(uesr_id: uesr_id, product_id: product_id)
-    Address.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, address: address, building: building, purchase_id: purchase_id)
+    Purchase.create(user_id: user_id, product_id: product_id)
+    Address.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, address: address, building: building, phone_number: phone_number, purchase_id: purchase_id)
   end
 end
